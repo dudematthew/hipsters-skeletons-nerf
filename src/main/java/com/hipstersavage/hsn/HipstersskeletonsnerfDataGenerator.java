a94aa4228ad479159f0480af8edfe283e909585e
Mateusz <mmoczydlowski4@gmail.com>
2024-01-11 22:17:33 +0100

    Remake project for IntelliJ

---
+++ src/main/java/com/hipstersavage/hsn/HipstersskeletonsnerfDataGenerator.java
@@ -0,0 +1,11 @@
+package com.hipstersavage.hsn;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HipstersskeletonsnerfDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
